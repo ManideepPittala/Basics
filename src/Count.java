7653cc2bc61c40c935960b16c1d47cc73c4b7768
mapittal <mapittal@inlen8520085366.corp.capgemini.com>
2023-04-19 13:08:18 +0530

    second commit

---
+++ src/Count.java
@@ -0,0 +1,45 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Count {
+
+
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+
+		int[] numbers= {1,2,3,2,1,4,5,4,3};
+
+		Map<Integer,Integer>countMap=new HashMap<>();
+
+		for(int number:numbers) {
+
+			if(countMap.containsKey(number)) {
+
+				countMap.put(number,countMap.get(number)+1);
+
+			}
+
+			else {
+
+				countMap.put(number,1);
+
+			}
+
+		}
+
+		System.out.println("Repeated values in the array:");
+
+		for(Map.Entry<Integer, Integer>entry:countMap.entrySet()) {
+
+			if(entry.getValue()>1) {
+
+				System.out.println(entry.getKey()+"-"+entry.getValue()+"times");
+
+			}
+
+		}
+	}
+
+}
---
+++ src/ReverseArray.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class ReverseArray {
+
+
+	public static void main(String[] args) {
+
+	
+
+		int[] arr={1,2,3,4,5};
+
+		System.out.println("Original Array:"+Arrays.toString(arr));
+
+		//reverse the array
+
+		for(int i=0;i<arr.length/2;i++) {
+
+			int temp=arr[i];
+
+			arr[i]=arr[arr.length-i-1];
+
+			arr[arr.length-i-1]=temp;
+
+		}
+
+		System.out.println("Reversed Array:"+Arrays.toString(arr));
+
+
+	}
+
+
+}
